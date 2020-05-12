51fe0ac8ab399f0903f9e0cb2adc96369bd958bb
gcharang <mrgcharang@gmail.com>
2020-05-12 18:40:24 +0530

    makes aya script work for both ubuntu18.04 and debian 10

--- build/aya.sh
+++ build/aya.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # AYA build script for Ubuntu & Debian 9 v.3 (c) Decker (and webworker)
-# Modifiled for Debian 10 by gcharang
+# Modified for Debian 10 by gcharang
 
 set -euxo pipefail
 #set -e
@@ -12,7 +12,13 @@ berkeleydb() {
     wget -N 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
     echo '12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef db-4.8.30.NC.tar.gz' | sha256sum -c
     tar -xzvf db-4.8.30.NC.tar.gz
-    sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' db-4.8.30.NC/dbinc/atomic.h
+    if [ -f /etc/debian_version ]; then
+        DEBIAN_VERSION=$(cat /etc/debian_version)
+        DEBIAN_VERSION=${DEBIAN_VERSION%.*}
+        if [ "$DEBIAN_VERSION" -eq 10 ]; then
+            sed -i 's/__atomic_compare_exchange/__atomic_compare_exchange_db/g' db-4.8.30.NC/dbinc/atomic.h
+        fi
+    fi
     cd db-4.8.30.NC/build_unix/
     ../dist/configure -enable-cxx -disable-shared -with-pic -prefix=$AYA_PREFIX
     make install
