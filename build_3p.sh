3b4d7d9643c565c592948fe1800713e5f976a6e0
gcharang <mrgcharang@gmail.com>
2020-05-12 20:22:09 +0530

    adds aya conf creation to 3p build script

--- build_3p.sh
+++ build_3p.sh
@@ -5,18 +5,31 @@ set -euxo pipefail
 PWD==$(pwd)
 
 AYA() {
-    cd ~
-    if [ -d AYAv2 ]; then
-        cd AYAv2
-        git checkout master
-        cd $PWD
-    else
-        git clone https://github.com/sillyghost/AYAv2.git -b master --single-branch
-        cd $PWD
-    fi
+	if [ ! -f ~/.aryacoin/aryacoin.conf ]; then
+		cat <<-EOF >~/.aryacoin/aryacoin.conf
+			server=1
+			daemon=1
+			txindex=1
+			rpcuser=user
+			rpcpassword=$(xxd -l 16 -p /dev/urandom)
+			bind=127.0.0.1
+			rpcbind=127.0.0.1
+			rpcallowip=127.0.0.1
+		EOF
+	fi
+	chmod 600 ~/.aryacoin/aryacoin.conf
+	cd ~
+	if [ -d AYAv2 ]; then
+		cd AYAv2
+		git checkout master
+		cd $PWD
+	else
+		git clone https://github.com/sillyghost/AYAv2.git -b master --single-branch
+		cd $PWD
+	fi
 
-    cp ./build/aya.sh ~/AYAv2/build.sh
-    cd ~/AYAv2
-    ./build.sh
+	cp ./build/aya.sh ~/AYAv2/build.sh
+	cd ~/AYAv2
+	./build.sh
 }
 AYA
