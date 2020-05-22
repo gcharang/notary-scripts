023290a10155221563ad72fa63b6b46084e536ef
gcharang <mrgcharang@gmail.com>
2020-05-22 22:51:59 +0530

    updates tmux conf file

--- 3p_tails_tmux.sh
+++ 3p_tails_tmux.sh
@@ -4,10 +4,6 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 #set -e
 
-tmux set -g set-titles on
-tmux setw -g window-status-current-style bg=colour0,fg=colour11,dim
-tmux set -g pane-border-status top
-tmux set-window-option -g window-status-current-bg blue
 tmux new-window -n 'tails'
 tmux split-window -v -t 0
 tmux select-pane -T komodo
