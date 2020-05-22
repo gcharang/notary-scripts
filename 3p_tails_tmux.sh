69e57417ec1bc69c9f99d6282e3c83267076a547
gcharang <mrgcharang@gmail.com>
2020-05-23 00:20:13 +0530

    updates tmux tails 'file

--- 3p_tails_tmux.sh
+++ 3p_tails_tmux.sh
@@ -6,19 +6,19 @@
 
 tmux new-window -n 'tails'
 tmux select-pane -t 0
-tmux split-window -v -t 0 'tmux select-pane -T AYA && tail -f ~/.komodo/debug.log'
+tmux split-window -v -t 0 'tmux select-pane -T KMD && tail -f ~/.komodo/debug.log'
 tmux select-pane -t 0
-tmux split-window -v -t 0 'tmux select-pane -T GAME && tail -f ~/.komodo/debug.log'
+tmux split-window -v -t 0 'tmux select-pane -T GAME && tail -f ~/.gamecredits/debug.log'
 tmux select-pane -t 0
-tmux split-window -v -t 0 'tmux select-pane -T GIN && tail -f ~/.komodo/debug.log'
+tmux split-window -v -t 0 'tmux select-pane -T GIN && tail -f ~/.gincoincore/debug.log'
 tmux select-pane -t 0
-tmux split-window -v -t 0 'tmux select-pane -T CHIPS && tail -f ~/.komodo/debug.log'
+tmux split-window -v -t 0 'tmux select-pane -T CHIPS && tail -f ~/.chips/debug.log'
 tmux select-pane -t 0
-tmux split-window -h -t 0 'tmux select-pane -T HUSH && tail -f ~/.komodo/debug.log'
+tmux split-window -h -t 0 'tmux select-pane -T HUSH && tail -f ~/.komodo/HUSH3/debug.log'
 tmux select-pane -t 0
-tmux split-window -h -t 0 'tmux select-pane -T VRSC && tail -f ~/.komodo/debug.log'
+tmux split-window -h -t 0 'tmux select-pane -T EMC2 && tail -f ~/.einsteinium/debug.log'
 tmux select-pane -t 0
-tmux split-window -h -t 0 'tmux select-pane -T KMD && tail -f ~/.komodo/debug.log'
+tmux split-window -h -t 0 'tmux select-pane -T AYA && tail -f ~/.aryacoin/debug.log'
 tmux select-pane -t 0
 tmux select-layout tiled
 #tmux select-pane -T title1
