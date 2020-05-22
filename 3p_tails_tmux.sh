462fe6806536680b72897a47562ecece247a283e
gcharang <mrgcharang@gmail.com>
2020-05-22 23:49:53 +0530

    updates tmux tails 'file

--- 3p_tails_tmux.sh
+++ 3p_tails_tmux.sh
@@ -5,9 +5,23 @@
 #set -e
 
 tmux new-window -n 'tails'
-tmux split-window -v -t 0
-tmux select-pane -T komodo
-tail -f ~/.komodo/debug.log
-tmux split-window -v -t 0 "tail -f ~/.komodo/debug.log"
+tmux split-window -v -t 0 'tmux select-pane -T AYA && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T GAME && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T GIN && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T CHIPS && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T HUSH && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T VRSC && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux split-window -v -t 0 'tmux select-pane -T KMD && tail -f ~/.komodo/debug.log'
+tmux select-pane -U
+tmux select-layout tiled
+#tmux select-pane -T title1
 
-tmux select-pane -T title1
+#tmux new-window \; split-window -v -l 25 \; split-window -h -l 70 \; select-pane -U \; split-window -h -l 70 \; select-pane -L
+#tmux new-window \; split-window -v -l 25 \; split-window -h -l 50 \; split-window -h -l 50 \; split-window -h -l 50 \; split-window -h -l 50 \; split-window -h -l 50 "tail -f ~/.komodo/debug.log"\;
+#tmux select-layout tiled
