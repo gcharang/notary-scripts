a560f0f4ed7a5313822aba181cf7c56c9f9f8c6f
gcharang <mrgcharang@gmail.com>
2020-05-30 19:37:46 +0530

    updates tmux install script

--- tmux_install_latest.sh
+++ tmux_install_latest.sh
@@ -4,7 +4,13 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 echo "this script kills your current tmux session if any. you have to kill the tmux server and restart it"
-
+echo "this is your chance to CTRL+C it"
+echo "waiting 5 seconds"
+echo "in 5..." && sleep 1
+echo "in 4..." && sleep 1
+echo "in 3..." && sleep 1
+echo "in 2..." && sleep 1
+echo "in 1..." && sleep 1
 set -euxo pipefail
 SCRIPT_DIR=$(pwd)
 sudo apt-get install libevent-dev build-essential bison pkg-config libncurses5-dev libncursesw5-dev
