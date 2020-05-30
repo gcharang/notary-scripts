8d69eaf9aebc89fe2ec529bd314804bef6f1c1fa
gcharang <mrgcharang@gmail.com>
2020-05-30 19:34:42 +0530

    updates tmux install script

--- tmux_install_latest.sh
+++ tmux_install_latest.sh
@@ -6,7 +6,7 @@
 echo "this script kills your current tmux session if any. you have to kill the tmux server and restart it"
 
 set -euxo pipefail
-PWD=$(pwd)
+SCRIPT_DIR=$(pwd)
 sudo apt-get install libevent-dev build-essential bison pkg-config libncurses5-dev libncursesw5-dev
 rm -fr /tmp/tmux
 mkdir -p /tmp/tmux
@@ -15,6 +15,6 @@ wget -qO- https://github.com/tmux/tmux/releases/download/3.1b/tmux-3.1b.tar.gz |
 cd tmux-3.1b
 ./configure
 make && sudo make install
-cp $PWD/dotfiles/.tmux.conf ~/.tmux.conf
+cp $SCRIPT_DIR/dotfiles/.tmux.conf ~/.tmux.conf
 rm -fr /tmp/tmux
 tmux kill-sess
