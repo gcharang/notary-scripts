76d5404305b8cd6d90e4defde28b167fb9dde729
gcharang <mrgcharang@gmail.com>
2020-06-13 05:53:48 +0530

    adds 3p_cli

---
+++ 3p_cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+komodo-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
+hush-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
+chips-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
+gamecredits-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
+einsteinium-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
+aryacoin-cli $1 $2 $3 $4 $5 $6 $7 $8 $9
