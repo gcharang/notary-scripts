c51136dca4e05320feb923ba5fda11907d774d44
gcharang <mrgcharang@gmail.com>
2020-06-13 22:20:37 +0530

    adds main cron

--- btcSplit.sh
+++ btcSplit.sh
@@ -17,12 +17,12 @@ inpool=$(jq -r '.data.mempool_transactions' <<<"${chair}")
 USD=$(jq -r '.data.market_price_usd' <<<"${chair}")
 TXFEE_SATOSHI_BYTE=$(bc <<<"${chairfee} + 1")
 if [[ $TXFEE_SATOSHI_BYTE -gt 65 ]]; then
-	echo "Fees too high now: ${TXFEE_SATOSHI_BYTE}" >>~/btc_split.log
+	echo "Fees too high now: ${TXFEE_SATOSHI_BYTE}"
 	exit 1
 fi
-echo "There are currently ${inpool} txes in the mempool" >>~/btc_split.log
-echo "BTC/USD: ${USD}" >>~/btc_split.log
-echo "Our fee(per byte): ${TXFEE_SATOSHI_BYTE}" >>~/btc_split.log
+echo "There are currently ${inpool} txes in the mempool"
+echo "BTC/USD: ${USD}"
+echo "Our fee(per byte): ${TXFEE_SATOSHI_BYTE}"
 
 UTXOs=$(curl -s "https://blockchain.info/unspent?active=${NN_ADDRESS}&limit=999" | jq '.unspent_outputs|[map(select(( (.value|tonumber) != 10000)))| .[] | {tx_hash_big_endian, tx_output_n, value,script}]')
 numNotaUTXOs=$(curl -s "https://blockchain.info/unspent?active=${NN_ADDRESS}&limit=999" | jq '.unspent_outputs|[map(select(( (.value|tonumber) = 10000)))| .[] ] | length')
@@ -36,7 +36,7 @@ SPLIT_TOTAL_SATOSHI=$(bc <<<"${SPLIT_VALUE}*${SPLIT_COUNT}*100000000")
 SPLIT_TOTAL_SATOSHI=("$(printf "%.0f" ${SPLIT_TOTAL_SATOSHI})")
 
 if [[ $numNotaUTXOs -gt 300 ]]; then
-	echo "Have: ${numNotaUTXOs} notaUTXOs" >>~/btc_split.log
+	echo "Have: ${numNotaUTXOs} notaUTXOs"
 	exit 1
 fi
 
@@ -49,7 +49,7 @@ if [[ $UTXOs != "[]" ]]; then
 	done
 	COINAGE_TOTAL=$(bc <<<"scale=8;${COINAGE_TOTAL_SATOSHI}/100000000")
 	for script in $(jq -r '.[].script' <<<"${UTXOs}"); do scripts+=("$script"); done
-	echo "Coinage total: ${COINAGE_TOTAL/#./0.} ($"$(bc <<<"scale=2;${COINAGE_TOTAL}*${USD}")")" >>~/btc_split.log
+	echo "Coinage total: ${COINAGE_TOTAL/#./0.} ($"$(bc <<<"scale=2;${COINAGE_TOTAL}*${USD}")")"
 	TOTAL_INS=${#vins[@]}
 	TOTAL_UTXOs=$(bc <<<"${TOTAL_INS} + ${SPLIT_COUNT}")
 	if [[ $TOTAL_UTXOs -gt 252 ]]; then
@@ -69,7 +69,7 @@ if [[ $UTXOs != "[]" ]]; then
 
 	rawtxsize=$(($(wc -m <<<${rawtx}) / 2))                                     # 2 chrs = 1 byte
 	rawtxsize=$(((rawtxsize + (${TOTAL_INS} * 107) + (SPLIT_COUNT * 44) + 38))) # inputs size + signatures + outputs size((8 + 1 + 35) each) + change size(8 + 1 + 25) + nLockTime (4)
-	echo "estimated TX size: ${rawtxsize}" >>~/btc_split.log
+	echo "estimated TX size: ${rawtxsize}"
 
 	for ((i = 1; i <= $SPLIT_COUNT; i++)); do
 		value=$(printf "%016x" ${SPLIT_VALUE_SATOSHI} | dd conv=swab 2>/dev/null | rev)
@@ -78,20 +78,20 @@ if [[ $UTXOs != "[]" ]]; then
 
 	TOTAL_FEE_SATOSHI=$((${rawtxsize} * ${TXFEE_SATOSHI_BYTE}))
 	TOTAL_FEE=$(bc <<<"scale=8;${TOTAL_FEE_SATOSHI}/100000000")
-	echo "TOTAL_FEE: ${TOTAL_FEE/#./0.} ($"$(bc <<<"scale=2;${TOTAL_FEE}*${USD}")")" >>~/btc_split.log
+	echo "TOTAL_FEE: ${TOTAL_FEE/#./0.} ($"$(bc <<<"scale=2;${TOTAL_FEE}*${USD}")")"
 	change_satoshis=$(bc <<<"${COINAGE_TOTAL_SATOSHI}-${SPLIT_TOTAL_SATOSHI}-${TOTAL_FEE_SATOSHI}")
 	change=$(bc <<<"scale=8;${change_satoshis}/100000000")
-	echo "Change: ${change/#./0.} (${change_satoshis})" >>~/btc_split.log
+	echo "Change: ${change/#./0.} (${change_satoshis})"
 
 	value=$(printf "%016x" ${change_satoshis} | dd conv=swab 2>/dev/null | rev)
 	rawtx="${rawtx}${value}1976a914${NN_HASH160}88ac00000000" # adding change amount and OP codes and for BTC nLockTime is 0 (!)
 
 	Signed=$(bitcoin-cli -stdin signrawtransaction <<<${rawtx} | jq -r '.hex')
 	lasttx=$(bitcoin-cli -stdin sendrawtransaction <<<${Signed})
-	echo -e "\033[33mSplit TX: ${lasttx}\033[0m" >>~/btc_split.log
-	echo -e "\n" >>~/btc_split.log
+	echo -e "\033[33mSplit TX: ${lasttx}\033[0m"
+	echo -e "\n"
 	exit 0
 else
-	echo -e "ERROR!\n Nothing to split ... :(" >>~/btc_split.log
+	echo -e "ERROR!\n Nothing to split ... :("
 	exit 1
 fi
