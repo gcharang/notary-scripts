a485e06dc45519cf6f5b27d3b1988a517609a844
gcharang <mrgcharang@gmail.com>
2020-06-14 13:55:48 +0530

    adds unsplit script

---
+++ unsplit_kmd_assets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source config
+
+echo "KMD"
+balance=$(komodo-cli getbalance)
+balance=$(echo "$balance/1" | bc)
+toSend=$((balance - 0.0001))
+txid=$(komodo-cli sendtoaddress $kmd_address $toSend)
+echo "merged $toSend coins in $txid"
+./listassetchains | while read chain; do
+    echo $chain
+    balance=$(komodo-cli -ac_name=$chain getbalance)
+    balance=$(echo "$balance/1" | bc)
+    toSend=$((balance - 0.0001))
+    txid=$(komodo-cli -ac_name=$chain sendtoaddress $kmd_address $toSend)
+    echo "merged $toSend coins in $txid"
+done
