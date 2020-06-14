af9848c1fd042efd20c0b82de791daa9e402f2cc
gcharang <mrgcharang@gmail.com>
2020-06-14 17:39:52 +0530

    update

--- unsplit_kmd_assets.sh
+++ unsplit_kmd_assets.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 source config
-
+cd ~/dPoW/iguana
 echo "KMD"
 balance=$(komodo-cli getbalance)
 balance=$(echo "$balance/1" | bc)
-toSend=$(echo "balance - 0.0001" | bc)
+toSend=$(echo "$balance - 0.0001" | bc)
 txid=$(komodo-cli sendtoaddress $kmd_address $toSend)
 echo "merged $toSend coins in $txid"
 ./listassetchains | while read chain; do
