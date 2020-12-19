47e2aca747ff69f68b7b33625f1ff0a29e2432c5
gcharang <mrgcharang@gmail.com>
2020-12-19 12:31:16 +0530

    adds coins

--- start_coins.sh
+++ start_coins.sh
@@ -14,17 +14,19 @@ elif [ "$main" = false ] && [ "$third_party" = true ]; then
     source ~/dPoW/iguana/pubkey.txt
     chipsd -pubkey=$pubkey &
     #gamecreditsd -pubkey=$pubkey &
+    powerblockcoind -pubkey=$pubkey &
     einsteiniumd -pubkey=$pubkey &
-    hushd -pubkey=$pubkey &
+    #hushd -pubkey=$pubkey &
     aryacoind -pubkey=$pubkey &
     verusd -pubkey=$pubkey &
-    #~/Marmara-v.1.0/src/komodod -ac_name=MCL -pubkey=$pubkey -ac_supply=2000000 -ac_cc=2 -addnode=37.148.210.158 -addnode=37.148.212.36 -addressindex=1 -spentindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000 &
+    ~/Marmara-v.1.0/src/komodod -ac_name=MCL -pubkey=$pubkey -ac_supply=2000000 -ac_cc=2 -addnode=37.148.210.158 -addnode=37.148.212.36 -addressindex=1 -spentindex=1 -ac_marmara=1 -ac_staked=75 -ac_reward=3000000000 &
+    gleecbtcd -pubkey=$pubkey &
     komodod -notary -pubkey=$pubkey &
 elif [ "$main" = true ] && [ "$third_party" = false ]; then
     cp -f ~/dPoW/iguana/pubkey.txt ~/komodo/src/pubkey.txt
     source ~/dPoW/iguana/pubkey.txt
     bitcoind &
-    komodod -gen -genproclimit=1 -notary -pubkey=$pubkey -minrelaytxfee=0.000035 -opretmintxfee=0.004 &
+    komodod -gen -genproclimit=2 -notary -pubkey=$pubkey -minrelaytxfee=0.000035 -opretmintxfee=0.004 &
     #komodod -notary -pubkey=$pubkey -minrelaytxfee=0.000035 -opretmintxfee=0.004 &
     cd ~/dPoW/iguana
     cp -f assetchains.old ~/komodo/src
